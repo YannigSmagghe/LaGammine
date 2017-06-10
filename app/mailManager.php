7e28af8b426b2076a35baf7a903d13419c2b9296
debyann <Yannigsmagghe>
2017-06-10 10:30:15 +0000

    mails

---
+++ app/mailManager.php
@@ -0,0 +1,115 @@
+<?php
+/*
+	********************************************************************************************
+	CONFIGURATION
+	********************************************************************************************
+*/
+// destinataire est votre adresse mail. Pour envoyer à plusieurs à la fois, séparez-les par une virgule
+$destinataire = 'yannig.smagghe@sfr.fr';
+
+// copie ? (envoie une copie au visiteur)
+$copie = 'oui'; // 'oui' ou 'non'
+
+// Messages de confirmation du mail
+$message_envoye = "Votre message nous est bien parvenu !";
+$message_non_envoye = "L'envoi du mail a échoué, veuillez réessayer SVP.";
+
+// Messages d'erreur du formulaire
+$message_erreur_formulaire = "";
+$message_formulaire_invalide = "";
+
+/*
+	********************************************************************************************
+	FIN DE LA CONFIGURATION
+	********************************************************************************************
+*/
+
+// on teste si le formulaire a été soumis
+if (!isset($_POST['envoi']))
+{
+	// formulaire non envoyé
+	echo '<p>'.$message_erreur_formulaire.'</p>'."\n";
+}
+else
+{
+
+	/*
+	 * cette fonction sert à nettoyer et enregistrer un texte
+	 */
+	function Rec($text)
+	{
+		$text = htmlspecialchars(trim($text), ENT_QUOTES);
+		if (1 === get_magic_quotes_gpc())
+		{
+			$text = stripslashes($text);
+		}
+
+		$text = nl2br($text);
+		return $text;
+	};
+
+	// formulaire envoyé, on récupère tous les champs.
+	$nom     = (isset($_POST['last']))     ? Rec($_POST['last'])     : '';
+	$prenom  = (isset($_POST['first']))     ? Rec($_POST['first'])     : '';
+	$email   = (isset($_POST['email']))   ? Rec($_POST['email'])   : '';
+	$objet   = ("Nouveau Message du Site La Gammine");
+	$tel     = (isset($_POST['tel'])) ? Rec($_POST['tel']) : '';
+	$message = (isset($_POST['message'])) ? Rec($_POST['message']) : '';
+
+
+	if (($nom != '') && ($prenom != '') && ($email != '') && ($message != '') && ($tel != ''))
+	{
+		// les 4 variables sont remplies, on génère puis envoie le mail
+		$headers  = 'MIME-Version: 1.0' . "\r\n";
+		$headers .= 'From:'.$nom.' <'.$email.'>' . "\r\n" .
+				'Reply-To:'.$email. "\r\n" .
+				'Content-Type: text/plain; charset="utf-8"; DelSp="Yes"; format=flowed '."\r\n" .
+				'Content-Disposition: inline'. "\r\n" .
+				'Content-Transfer-Encoding: 7bit'." \r\n" .
+				'X-Mailer:PHP/'.phpversion();
+
+		// envoyer une copie au visiteur ?
+		if ($copie == 'oui')
+		{
+			$cible = $destinataire.';'.$email;
+		}
+		else
+		{
+			$cible = $destinataire;
+		};
+
+		// Remplacement de certains caractères spéciaux
+		$message = str_replace("&#039;","'",$message);
+		$message = str_replace("&#8217;","'",$message);
+		$message = str_replace("&quot;",'"',$message);
+		$message = str_replace('<br>','',$message);
+		$message = str_replace('<br />','',$message);
+		$message = str_replace("&lt;","<",$message);
+		$message = str_replace("&gt;",">",$message);
+		$message = str_replace("&amp;","&",$message);
+
+		// Envoi du mail
+		$num_emails = 0;
+		$tmp = explode(';', $cible);
+		foreach($tmp as $email_destinataire)
+		{
+			if (mail($email_destinataire, $objet, $message, $headers))
+				$num_emails++;
+		}
+
+		if ((($copie == 'oui') && ($num_emails == 2)) || (($copie == 'non') && ($num_emails == 1)))
+		{
+			echo '<p>'.$message_envoye.'</p>';
+		}
+		else
+		{
+			echo '<p>'.$message_non_envoye.'</p>';
+		};
+	}
+	else
+	{
+		// une des 3 variables (ou plus) est vide ...
+		echo '<p>'.$message_formulaire_invalide.' <a href="contact.html">Retour au formulaire</a></p>'."\n";
+	};
+}; // fin du if (!isset($_POST['envoi']))
+?>
--- app/scripts/custom.js
+++ app/scripts/custom.js
@@ -24,4 +24,5 @@ $(document).ready(function() {
       $('.list-date li:last').after(appendItem);
     }
   }
+
 });
